16133c7446510152171d9b9ce562cae5a1d54b11
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:45:11 +0200

    Create programtype.d

---
+++ source/models/educations/entities/students/programtype.d
@@ -0,0 +1,6 @@
+module models.educations.entities.students.programtype;
+
+@safe:
+import models.educations;
+
+// The master list of program types defined by the institution, such as major, minor, or concentration.
