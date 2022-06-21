6bb0fca8179dfa6a95c50a5f1fd745140b3dbe13
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 07:38:35 +0200

    Update academicperiod.d

--- source/models/education/entities/academicperiod.d
+++ source/models/education/entities/academicperiod.d
@@ -2,3 +2,5 @@ module models.education.entities.academicperiod;
 
 @safe:
 import models.education;
+
+// The periods of time in which courses are offered by the institution, such as a term or semester.
