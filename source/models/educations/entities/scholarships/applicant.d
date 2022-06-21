45da41773c27dc3f3d5cc753b79c713a9aed4f84
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:42:41 +0200

    Create applicant.d

---
+++ source/models/educations/entities/scholarships/applicant.d
@@ -0,0 +1,6 @@
+module models.educations.entities.scholarships.applicant;
+
+@safe:
+import models.educations;
+
+// This entity is used as an scholarship application form and to store the list of students who have applied to a particular scholarship.
