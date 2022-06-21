564f72a35f818bc48eeaf14fe54040198019749c
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:40:51 +0200

    Create registrationstatus.d

---
+++ source/models/educations/entities/registrationstatus.d
@@ -0,0 +1,6 @@
+module models.educations.entities.registrationstatus;
+
+@safe:
+import models.educations;
+
+// The master list of registration statuses tracked by the institution. The registration status is associated to the student course history.
