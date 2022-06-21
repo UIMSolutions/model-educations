92c59ebb1d4af0bae4f7ece201b854b0b4f51db1
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:16:00 +0200

    Create internship.d

---
+++ source/models/educations/entities/internships/internship.d
@@ -0,0 +1,6 @@
+module models.educations.entities.internships.internship;
+
+@safe:
+import models.educations;
+
+// 	This entity stores the list of internships offered to a school by all business partners.
