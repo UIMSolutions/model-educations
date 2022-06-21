a2c7a2598e6edbbc7a24641569a8861935ce8a29
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:12:38 +0200

    Create applicant.d

---
+++ source/models/educations/entities/grants/applicant.d
@@ -0,0 +1,6 @@
+module models.educations.entities.grants.applicant;
+
+@safe:
+import models.educations;
+
+// This entity is used as an internship application form and to store the list of students who have applied to a particular internship.
