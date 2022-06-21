7eff9de4a1bdbfca65814302ec11c5d7d0e37b40
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 07:56:53 +0200

    Update areaofstudy.d

--- source/models/educations/entities/areaofstudy.d
+++ source/models/educations/entities/areaofstudy.d
@@ -2,3 +2,5 @@ module models.educations.entities.areaofstudy;
 
 @safe:
 import models.educations;
+
+// The master list of all area of studies offered by an institution. Areas of studies are more specific program offerings, such as Master's of Public Administration - Emergency Management.
