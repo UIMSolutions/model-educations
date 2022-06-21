75158adcacb4af1c6ca0e82817ae48922b08ee3b
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:02:12 +0200

    Create educationlevel.d

---
+++ source/models/educations/entities/educationlevel.d
@@ -0,0 +1,6 @@
+module models.educations.entities.educationlevel;
+
+@safe:
+import models.educations;
+
+// The master list of all education levels mapped in the external system, such as Associate of Arts, Bachelor, Certificate.
