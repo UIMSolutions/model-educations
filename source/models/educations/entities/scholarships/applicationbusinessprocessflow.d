6ba9fd691d4132ad4765177070a9aa6bc04387e2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 08:43:08 +0200

    Create applicationbusinessprocessflow.d

---
+++ source/models/educations/entities/scholarships/applicationbusinessprocessflow.d
@@ -0,0 +1,6 @@
+module models.educations.entities.scholarships.applicationbusinessprocessflow;
+
+@safe:
+import models.educations;
+
+// Base entity for process Scholarship Application Business Process Flow
