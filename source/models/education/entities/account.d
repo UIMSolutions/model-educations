89acb2f51ab0a7704accf98b004fe9d1c3798773
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-21 07:41:03 +0200

    Create account.d

---
+++ source/models/education/entities/account.d
@@ -0,0 +1,6 @@
+module models.education.entities.account;
+
+@safe:
+import models.education;
+
+// Business that represents a customer or potential customer. The company that is billed in business transactions.
