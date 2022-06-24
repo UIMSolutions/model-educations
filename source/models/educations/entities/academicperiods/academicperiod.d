2c9829eacaaa3f9fc26125ea99b7720236b376a1
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-06-24 09:09:56 +0200

    Update package academicperiods

--- source/models/educations/entities/academicperiods/academicperiod.d
+++ source/models/educations/entities/academicperiods/academicperiod.d
@@ -4,3 +4,53 @@ module models.educations.entities.academicperiods.academicperiod;
 import models.educations;
 
 // The periods of time in which courses are offered by the institution, such as a term or semester.
+class DAcademicPeriodEntity : DOOPEntity {
+  mixin(EntityThis!("AcademicPeriodEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+        "createdOn": StringAttribute, //	Date and time when the record was created.	higherEducation/AcademicPeriod
+        "createdBy": StringAttribute, //	Shows who created the record.	higherEducation/AcademicPeriod
+        "modifiedOn": StringAttribute, //	Date and time when the record was modified.	higherEducation/AcademicPeriod
+        "modifiedBy": StringAttribute, //	Shows who last updated the record.	higherEducation/AcademicPeriod
+        "createdOnBehalfBy": StringAttribute, //	Shows who created the record on behalf of another user.	higherEducation/AcademicPeriod
+        "modifiedOnBehalfBy": StringAttribute, //	Shows who last updated the record on behalf of another user.	higherEducation/AcademicPeriod
+        "overriddenCreatedOn": StringAttribute, //	Date and time that the record was migrated.	higherEducation/AcademicPeriod
+        "importSequenceNumber": StringAttribute, //	Unique identifier of the data import or data migration that created this record.	higherEducation/AcademicPeriod
+        "ownerId": StringAttribute, //	Owner Id	higherEducation/AcademicPeriod
+        "ownerIdType": StringAttribute, //	The type of owner, either User or Team.	higherEducation/AcademicPeriod
+        "owningBusinessUnit": StringAttribute, //	Unique identifier for the business unit that owns the record	higherEducation/AcademicPeriod
+        "owningUser": StringAttribute, //	Unique identifier of the user that owns the activity.	higherEducation/AcademicPeriod
+        "owningTeam": StringAttribute, //	Unique identifier for the team that owns the record.	higherEducation/AcademicPeriod
+        "timeZoneRuleVersionNumber": StringAttribute, //	For internal use only.	higherEducation/AcademicPeriod
+        "UTCConversionTimeZoneCode": StringAttribute, //	Time zone code that was in use when the record was created.	higherEducation/AcademicPeriod
+        "versionNumber": StringAttribute, //	Version Number	higherEducation/AcademicPeriod
+        "academicPeriodId": StringAttribute, //	Unique identifier for entity instances	higherEducation/AcademicPeriod
+        "stateCode": StringAttribute, //	Status of the Academic Period	higherEducation/AcademicPeriod
+        "stateCode_display": StringAttribute, //		higherEducation/AcademicPeriod
+        "statusCode": StringAttribute, //	Reason for the status of the Academic Period	higherEducation/AcademicPeriod
+        "statusCode_display": StringAttribute, //		higherEducation/AcademicPeriod
+        "name": StringAttribute, //	The name of the academic period.	higherEducation/AcademicPeriod
+        "code": StringAttribute, //	Abbreviated code for the academic period.	higherEducation/AcademicPeriod
+        "endDate": StringAttribute, //		higherEducation/AcademicPeriod
+        "externalIdentifier": StringAttribute, //	External Identifier	higherEducation/AcademicPeriod
+        "externalSourceSystem": StringAttribute, //	External Source System	higherEducation/AcademicPeriod
+        "externalSourceSystem_display": StringAttribute, //		higherEducation/AcademicPeriod
+        "startDate": StringAttribute //		higherEducation/AcademicPeriod
+      ])
+      .registerPath("education_academicperiods");
+  }
+}
+mixin(EntityCalls!("AcademicPeriodEntity"));
+
+
+version(test_model_education) {
+  unittest {
+    assert(AcademicPeriodEntity);
+
+    auto entity = AcademicPeriodEntity;
+  }
+}
--- source/models/educations/entities/academicperiods/detail.d
+++ source/models/educations/entities/academicperiods/detail.d
@@ -4,3 +4,66 @@ module models.educations.entities.academicperiods.detail;
 import models.educations;
 
 // Status between student and their academic periods.
+class DAcademicPeriodDetailEntity : DOOPEntity {
+  mixin(EntityThis!("AcademicPeriodDetailEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+        "createdOn": StringAttribute, //	Date and time when the record was created.	higherEducation/AcademicPeriodDetail
+        "createdBy": StringAttribute, //	Shows who created the record.	higherEducation/AcademicPeriodDetail
+        "modifiedOn": StringAttribute, //	Date and time when the record was modified.	higherEducation/AcademicPeriodDetail
+        "modifiedBy": StringAttribute, //	Shows who last updated the record.	higherEducation/AcademicPeriodDetail
+        "createdOnBehalfBy": StringAttribute, //	Shows who created the record on behalf of another user.	higherEducation/AcademicPeriodDetail
+        "modifiedOnBehalfBy": StringAttribute, //	Shows who last updated the record on behalf of another user.	higherEducation/AcademicPeriodDetail
+        "overriddenCreatedOn": StringAttribute, //	Date and time that the record was migrated.	higherEducation/AcademicPeriodDetail
+        "importSequenceNumber": StringAttribute, //	Unique identifier of the data import or data migration that created this record.	higherEducation/AcademicPeriodDetail
+        "ownerId": StringAttribute, //	Owner Id	higherEducation/AcademicPeriodDetail
+        "ownerIdType": StringAttribute, //	The type of owner, either User or Team.	higherEducation/AcademicPeriodDetail
+        "owningBusinessUnit": StringAttribute, //	Unique identifier for the business unit that owns the record	higherEducation/AcademicPeriodDetail
+        "owningUser": StringAttribute, //	Unique identifier of the user that owns the activity.	higherEducation/AcademicPeriodDetail
+        "owningTeam": StringAttribute, //	Unique identifier for the team that owns the record.	higherEducation/AcademicPeriodDetail
+        "timeZoneRuleVersionNumber": StringAttribute, //	For internal use only.	higherEducation/AcademicPeriodDetail
+        "UTCConversionTimeZoneCode": StringAttribute, //	Time zone code that was in use when the record was created.	higherEducation/AcademicPeriodDetail
+        "versionNumber": StringAttribute, //	Version Number	higherEducation/AcademicPeriodDetail
+        "academicPeriodDetailsId": StringAttribute, //	Unique identifier for entity instances	higherEducation/AcademicPeriodDetail
+        "stateCode": StringAttribute, //	Status of the Academic Period Detail	higherEducation/AcademicPeriodDetail
+        "stateCode_display": StringAttribute, //		higherEducation/AcademicPeriodDetail
+        "statusCode": StringAttribute, //	Reason for the status of the Academic Period Detail	higherEducation/AcademicPeriodDetail
+        "statusCode_display": StringAttribute, //		higherEducation/AcademicPeriodDetail
+        "name": StringAttribute, //	The name of the custom entity.	        higherEducation/AcademicPeriodDetail": StringAttribute, //
+        "academicPeriodID": StringAttribute, //		higherEducation/AcademicPeriodDetail
+        "attendanceType": StringAttribute, //	The enrollment attendance type for the student.	higherEducation/AcademicPeriodDetail
+        "attendanceType_display": StringAttribute, //		higherEducation/AcademicPeriodDetail
+        "creditLoad": StringAttribute, //	Assigned credit load for academic period	higherEducation/AcademicPeriodDetail
+        "educationLevelId": StringAttribute, //	The education level for the student.	higherEducation/AcademicPeriodDetail
+        "employedHoursPerWeek": StringAttribute, //	Employed for how many hours/week?	higherEducation/AcademicPeriodDetail
+        "isAthlete": StringAttribute, //	Athlete	higherEducation/AcademicPeriodDetail
+        "isEmployed": StringAttribute, //	Employed	higherEducation/AcademicPeriodDetail
+        "isInternationalStudent": StringAttribute, //	International Student	higherEducation/AcademicPeriodDetail
+        "ISIRonFIle": StringAttribute, //	ISIR received and on file?	higherEducation/AcademicPeriodDetail
+        "isTransferStudent": StringAttribute, //	Transfer Student	higherEducation/AcademicPeriodDetail
+        "majorCreditEarned": StringAttribute, //	Major Credit Earned	higherEducation/AcademicPeriodDetail
+        "majorGPA": StringAttribute, //	Major GPA	higherEducation/AcademicPeriodDetail
+        "midTermDeficiency": StringAttribute, //	Mid-Term Deficiency	higherEducation/AcademicPeriodDetail
+        "programId": StringAttribute, //	The program for the student.	higherEducation/AcademicPeriodDetail
+        "studentId": StringAttribute, //	Lookup to Student (Contact).	higherEducation/AcademicPeriodDetail
+        "termCreditsEarned": StringAttribute, //	Term Credits Earned	higherEducation/AcademicPeriodDetail
+        "termGPA": StringAttribute, //	Term GPA	higherEducation/AcademicPeriodDetail
+
+      ])
+      .registerPath("education_academicperioddetails");
+  }
+}
+mixin(EntityCalls!("AcademicPeriodDetailEntity"));
+
+
+version(test_model_education) {
+  unittest {
+    assert(AcademicPeriodDetailEntity);
+
+    auto entity = AcademicPeriodDetailEntity;
+  }
+}
