0f2950e5a911c22483529afe2e7e5e57dce7aa20
UI-Manufaktur <ui.manufaktur@gmail.com>
2022-06-27 09:59:11 +0200

    Updates in package extracurriculars

--- source/models/educations/entities/extracurriculars/activity.d
+++ source/models/educations/entities/extracurriculars/activity.d
@@ -4,3 +4,54 @@ module models.educations.entities.extracurriculars.activity;
 import models.educations;
 
 // The master list of all extra curricular activities tracked by the institution.
+class DExtraCurricularActivityEntity : DOOPEntity {
+  mixin(EntityThis!("ExtraCurricularActivityEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+        "createdOn": StringAttribute, //	Date and time when the record was created.	higherEducation/CourseSection
+        "createdBy": StringAttribute, //	Shows who created the record.	higherEducation/CourseSection
+        "modifiedOn": StringAttribute, //	Date and time when the record was modified.	higherEducation/CourseSection
+        "modifiedBy": StringAttribute, //	Shows who last updated the record.	higherEducation/CourseSection
+        "createdOnBehalfBy": StringAttribute, //	Shows who created the record on behalf of another user.	higherEducation/CourseSection
+        "modifiedOnBehalfBy": StringAttribute, //	Shows who last updated the record on behalf of another user.	higherEducation/CourseSection
+        "overriddenCreatedOn": StringAttribute, //	Date and time that the record was migrated.	higherEducation/CourseSection
+        "importSequenceNumber": StringAttribute, //	Unique identifier of the data import or data migration that created this record.	higherEducation/CourseSection
+        "ownerId": StringAttribute, //	Owner Id	higherEducation/CourseSection
+        "ownerIdType": StringAttribute, //	The type of owner, either User or Team.	higherEducation/CourseSection
+        "owningBusinessUnit": StringAttribute, //	Unique identifier for the business unit that owns the record	higherEducation/CourseSection
+        "owningUser": StringAttribute, //	Unique identifier of the user that owns the activity.	higherEducation/CourseSection
+        "owningTeam": StringAttribute, //	Unique identifier for the team that owns the record.	higherEducation/CourseSection
+        "timeZoneRuleVersionNumber": StringAttribute, //	For internal use only.	higherEducation/CourseSection
+        "UTCConversionTimeZoneCode": StringAttribute, //	Time zone code that was in use when the record was created.	higherEducation/CourseSection
+        "versionNumber": StringAttribute, //	Version Number	higherEducation/CourseSection
+        "courseSectionId": StringAttribute, //	Unique identifier for entity instances	higherEducation/CourseSection
+        "stateCode": StringAttribute, //	Status of the Course Section	higherEducation/CourseSection
+        "stateCode_display": StringAttribute, //		higherEducation/CourseSection
+        "statusCode": StringAttribute, //	Reason for the status of the Course Section	higherEducation/CourseSection
+        "statusCode_display": StringAttribute, //		higherEducation/CourseSection
+        "name": StringAttribute, //	The name of the custom entity.	higherEducation/CourseSection
+        "academicPeriodId": StringAttribute, //	Lookup to Academic Period.	higherEducation/CourseSection
+        "campusId": StringAttribute, //	Campus Location of the course.	higherEducation/CourseSection
+        "courseDayTime": StringAttribute, //	Text field to store the date and time of the course.	higherEducation/CourseSection
+        "courseId": StringAttribute, //	Lookup to Course	higherEducation/CourseSection
+        "instructorId": StringAttribute, //	Instructor	higherEducation/CourseSection
+        "location": StringAttribute, //	Specific Meeting Location	higherEducation/CourseSection
+        "prerequisites": StringAttribute, //	Prerequisites	higherEducation/CourseSection
+      ])
+      .registerPath("education_courses.histories");
+  }
+}
+mixin(EntityCalls!("ExtraCurricularActivityEntity"));
+
+
+version(test_model_education) {
+  unittest {
+    assert(ExtraCurricularActivityEntity);
+
+    auto entity = ExtraCurricularActivityEntity;
+  }
+}
--- source/models/educations/entities/extracurriculars/participant.d
+++ source/models/educations/entities/extracurriculars/participant.d
@@ -4,3 +4,57 @@ module models.educations.entities.extracurriculars.participant;
 import models.educations;
 
 // The relationship between a Contact and a specific Extra Curricular Activity.
+class DExtraCurricularParticipantEntity : DOOPEntity {
+  mixin(EntityThis!("ExtraCurricularParticipantEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+        "createdOn": StringAttribute, //	Date and time when the record was created.	higherEducation/ExtraCurricularParticipant
+        "createdBy": StringAttribute, //	Shows who created the record.	higherEducation/ExtraCurricularParticipant
+        "modifiedOn": StringAttribute, //	Date and time when the record was modified.	higherEducation/ExtraCurricularParticipant
+        "modifiedBy": StringAttribute, //	Shows who last updated the record.	higherEducation/ExtraCurricularParticipant
+        "createdOnBehalfBy": StringAttribute, //	Shows who created the record on behalf of another user.	higherEducation/ExtraCurricularParticipant
+        "modifiedOnBehalfBy": StringAttribute, //	Shows who last updated the record on behalf of another user.	higherEducation/ExtraCurricularParticipant
+        "overriddenCreatedOn": StringAttribute, //	Date and time that the record was migrated.	higherEducation/ExtraCurricularParticipant
+        "importSequenceNumber": StringAttribute, //	Unique identifier of the data import or data migration that created this record.	higherEducation/ExtraCurricularParticipant
+        "ownerId": StringAttribute, //	Owner Id	higherEducation/ExtraCurricularParticipant
+        "ownerIdType": StringAttribute, //	The type of owner, either User or Team.	higherEducation/ExtraCurricularParticipant
+        "owningBusinessUnit": StringAttribute, //	Unique identifier for the business unit that owns the record	higherEducation/ExtraCurricularParticipant
+        "owningUser": StringAttribute, //	Unique identifier of the user that owns the activity.	higherEducation/ExtraCurricularParticipant
+        "owningTeam": StringAttribute, //	Unique identifier for the team that owns the record.	higherEducation/ExtraCurricularParticipant
+        "timeZoneRuleVersionNumber": StringAttribute, //	For internal use only.	higherEducation/ExtraCurricularParticipant
+        "UTCConversionTimeZoneCode": StringAttribute, //	Time zone code that was in use when the record was created.	higherEducation/ExtraCurricularParticipant
+        "versionNumber": StringAttribute, //	Version Number	higherEducation/ExtraCurricularParticipant
+        "extracurricularParticipantId": StringAttribute, //	Unique identifier for entity instances	higherEducation/ExtraCurricularParticipant
+        "stateCode": StringAttribute, //	Status of the Extra Curricular Participant	higherEducation/ExtraCurricularParticipant
+        "stateCode_display": StringAttribute, //		higherEducation/ExtraCurricularParticipant
+        "statusCode": StringAttribute, //	Reason for the status of the Extra Curricular Participant	higherEducation/ExtraCurricularParticipant
+        "statusCode_display": StringAttribute, //		higherEducation/ExtraCurricularParticipant
+        "name": StringAttribute, //	The name of the custom entity.	higherEducation/ExtraCurricularParticipant
+        "awardsReceived": StringAttribute, //	Awards received at the Extra Curricular Activity or Club.	higherEducation/ExtraCurricularParticipant
+        "contactId": StringAttribute, //	Lookup to Student (Contact).	higherEducation/ExtraCurricularParticipant
+        "endDate": StringAttribute, //	End Date	higherEducation/ExtraCurricularParticipant
+        "extraCurricularActivityId": StringAttribute, //	Lookup to Extra Curricular	higherEducation/ExtraCurricularParticipant
+        "positionsHeld": StringAttribute, //	Positions held at the Extra Curricular Activity or Club	higherEducation/ExtraCurricularParticipant
+        "source": StringAttribute, //	Source of the student's extra curricular activity record.	higherEducation/ExtraCurricularParticipant
+        "source_display": StringAttribute, //		higherEducation/ExtraCurricularParticipant
+        "startDate": StringAttribute, //	Start Date	higherEducation/ExtraCurricularParticipant
+        "state": StringAttribute, //		higherEducation/ExtraCurricularParticipant
+        "state_display": StringAttribute, //		higherEducation/ExtraCurricularParticipant
+      ])
+      .registerPath("education_extracurriculars.participants");
+  }
+}
+mixin(EntityCalls!("ExtraCurricularParticipantEntity"));
+
+
+version(test_model_education) {
+  unittest {
+    assert(ExtraCurricularParticipantEntity);
+
+    auto entity = ExtraCurricularParticipantEntity;
+  }
+}
