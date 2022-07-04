2478dd322c00a1f855363872f2aa21d168d1f0c2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-04 20:53:09 +0200

    Update programtype.d

--- source/models/educations/entities/students/programtype.d
+++ source/models/educations/entities/students/programtype.d
@@ -4,6 +4,7 @@ module models.educations.entities.students.programtype;
 import models.educations;
 
 // The master list of program types defined by the institution, such as major, minor, or concentration.
+// name = name of the program type.	      
 class DStudentProgramTypeEntity : DOOPEntity {
   mixin(EntityThis!("StudentProgramTypeEntity"));
   
@@ -11,6 +12,9 @@ class DStudentProgramTypeEntity : DOOPEntity {
     super.initialize;
 
     this
+      .addValues([
+        OwnerIdAttribute, //	Owner Id	
+       ])
       .addValues([
         "createdOn": StringAttribute, //	Date and time when the record was created.	
         "createdBy": StringAttribute, //	Shows who created the record.	
@@ -20,20 +24,17 @@ class DStudentProgramTypeEntity : DOOPEntity {
         "modifiedOnBehalfBy": StringAttribute, //	Shows who last updated the record on behalf of another user.	
         "overriddenCreatedOn": StringAttribute, //	Date and time that the record was migrated.	
         "importSequenceNumber": StringAttribute, //	Unique identifier of the data import or data migration that created this record.	
-        "ownerId": StringAttribute, //	Owner Id	
         "ownerIdType": StringAttribute, //	The type of owner, either User or Team.	
         "owningBusinessUnit": StringAttribute, //	Unique identifier for the business unit that owns the record	
         "owningUser": StringAttribute, //	Unique identifier of the user that owns the activity.	
         "owningTeam": StringAttribute, //	Unique identifier for the team that owns the record.	
         "timeZoneRuleVersionNumber": StringAttribute, //	For internal use only.	
         "UTCConversionTimeZoneCode": StringAttribute, //	Time zone code that was in use when the record was created.	
-        "versionNumber": StringAttribute, //	Version Number	
         "studentProgramTypeId": StringAttribute, //	Unique identifier for entity instances	
         "stateCode": StringAttribute, //	Status of the Student Program Type	
         "stateCode_display": StringAttribute, //		
         "statusCode": StringAttribute, //	Reason for the status of the Student Program Type	
         "statusCode_display": StringAttribute, //		
-        "name": StringAttribute, //	The name of the program type.	      
       ])
       .registerPath("education_students.statuses");
   }
