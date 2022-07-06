331c233119fcfd62cddc1ed46ceb17b548f41d84
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-06 16:35:04 +0200

    Update package.d

--- source/models/educations/entities/package.d
+++ source/models/educations/entities/package.d
@@ -1,2 +1,11 @@
 module models.educations.entities;
 
+public import models.educations.entities.accomplishment;
+public import models.educations.entities.account;
+public import models.educations.entities.address;
+public import models.educations.entities.areaofinterest;
+public import models.educations.entities.areaofstudy;
+public import models.educations.entities.contact;
+public import models.educations.entities.educationlevel;
+public import models.educations.entities.previouseducation;
+public import models.educations.entities.registrationstatus;
